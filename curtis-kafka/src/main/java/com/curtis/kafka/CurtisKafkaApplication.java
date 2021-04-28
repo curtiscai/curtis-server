ad23a37379578aceb9a977f56d592d09ba302b80
curtis <375577020@qq.com>
2021-04-29 01:29:26 +0800

    init project

---
+++ curtis-kafka/src/main/java/com/curtis/kafka/CurtisKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.curtis.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurtisKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurtisKafkaApplication.class, args);
+    }
+
+}
